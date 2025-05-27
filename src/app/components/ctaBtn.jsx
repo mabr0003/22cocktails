be372dd684602a1dd177837b4d6efeddbeeb0fb5
Marcus <mabr0003@stud.kea.dk>
2025-05-27 11:14:18 +0200

    btn work

--- src/app/components/ctaBtn.jsx
+++ src/app/components/ctaBtn.jsx
@@ -1,6 +1,6 @@
 const CtaBtn = () => {
   return (
-    <a className="amatic self-end bg-[var(--action)] text-white text-2xl px-6 py-2 -mb-2 border-2 border-[var(--action)] hover:bg-transparent hover:text-[var(--text)] hover:font-bold transition-all duration-150" href="#">
+    <a className="amatic self-end bg-[var(--action)] text-white text-2xl px-6 py-2 -mb-2 border-2 border-[var(--action)] hover:bg-transparent hover:text-[var(--text)] hover:font-bold transition-all duration-150" href="#contact">
       Kontakt os nu
     </a>
   );
